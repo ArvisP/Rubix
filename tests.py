430d2b11cfa480069c510bd7445f908858f59464
Edwin Zhou <edwinzzhou@gmail.com>
2017-11-06 01:43:35 -0500

    Add first test case and altered config to suppress sqlalchemy error

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from app import app
 import unittest
 
-class RubixTests(unittest.TestCase):
+class RubixTestCase(unittest.TestCase):
 
   # init logic for rest of test suite declared in test module
   # code that is execeuted before all tests in one test run
@@ -26,13 +26,22 @@ class RubixTests(unittest.TestCase):
     pass
 
   # test method
-  def test_home_status_code(self):
-    # sends HTTP GET request to the application
-    # on the specified path
-    result = self.app.get('/')
+  def test_index(self):
 
-    # assert the status code of the response
-    self.assertEqual(result.status_code, 200)
+    # Ensure that flask was set up correctly
+    def test_index(self):
+      tester = app.test_client(self)
+      response = tester.get('/login', content_type='html/text')
+      self.assertEqual(response.status_code, 200)
+
+
+  # def test_home_status_code(self):
+  #   # sends HTTP GET request to the application
+  #   # on the specified path
+  #   result = self.app.get('/')
+
+  #   # assert the status code of the response
+  #   self.assertEqual(result.status_code, 200)
 
 if __name__ == '__main__':
   unittest.main()

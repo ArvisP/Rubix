40f625c5f3f90a2cd4624e1f40c80bbfab054c8f
ArvisP <arvis.peralta@gmail.com>
2017-11-07 11:19:08 -0500

    Finished event announcements, started event schedule

--- app/routes.py
+++ app/routes.py
@@ -81,16 +81,21 @@ def profile():
 
 @app.route('/learnmore')
 def learnmore():
-  return render_template('learnmore.html')
+    return render_template('learnmore.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
-@app.route('/eventselected')
-def eventselected():
-    return render_template('event.html', eventName="City College Cube Day")
+@app.route('/event-announcements')
+def eventAnnouncements():
+    events = [('Event 4', 'Announcement 4', 'Today 7:30PM'),('Event 3', 'Announcement 3', 'Today 7:00PM'),
+    ('Event 2', 'Announcement 2', 'Today 6:45PM'),('Event 1', 'Announcement 1', 'Today 6:30PM')]
+    return render_template('event-announcements.html', eventName="City College Cube Day", list = events, name="Arvis", eventorganizer=True)
 
+@app.route('/event-schedule')
+def eventSchedule():
+    return render_template('event-schedule.html', eventName="City College Cube Day")
 # @app.route('/signup')
 # def signup():
 #   return "hellowordl!"

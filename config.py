c53450cd7a2a05c9d6fe45823014dd31ace485e7
Ichwan Palongengi <ipalongengi@gmail.com>
2017-11-28 00:57:00 -0500

    create a new config for heroku deployment

--- config.py
+++ config.py
@@ -9,8 +9,15 @@ class BaseConfig(object):
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class TestConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+class HerokuDeployConfig(object):
+    SECRET_KEY = 'thisPasswordIsSoDarnAmazinglyStrong123!'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

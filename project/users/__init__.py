41a2aa6f33930b3e835cf0efb80f7a683d3661fe
ArvisP <arvis.peralta@gmail.com>
2017-12-02 18:20:41 -0500

    error

--- project/users/__init__.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from app.models import User
-from flask_login import LoginManager
-lm = LoginManager()
-lm.init_app(app)
-
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
